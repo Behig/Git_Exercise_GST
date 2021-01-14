14e06a3ec99cbd993f5f46765bb9edd64d5e6e82
Mohammad Goudarzi <mohammadgoudarzimoghadam@gmail.com>
2021-01-14 12:10:41 -0800

    Fix the syntax bug

--- test.cpp
+++ test.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello Amin, Mohammad";
-    std::cout << "Hello World!";
-	std::cout << "And Hello software engineeres and developers and students!" std::endl;
+	std::cout << "Hello Amin, Mohammad ";
+    std::cout << "Hello World! ";
+	std::cout << "And Hello software engineeres and developers and students!";
     return 0;
 	return 1;
 }
