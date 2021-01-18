9402900d030625a962bbd19bb9b6357842bde380
Amin <mejri@imr.uni-hannover.de>
2021-01-18 01:22:00 +0100

    Amins changes

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
-	std::cout << "And Hello software engineeres and developers and students!" std::endl;
+    std::cout << "Hi Everybody!";
+	std::cout << "And Hello software engineeres, developers and students!" std::endl;
     return 0;
+	# End
 }
